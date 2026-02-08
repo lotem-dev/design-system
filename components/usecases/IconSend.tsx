b6d43b2370e3ec690ad86451d7ec5da7add48cda
lotem-dev <lotem@jit.io>
2026-02-08 20:52:29 +0200

    Add files via upload

---
+++ components/usecases/IconSend.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+/** Usecase icon */
+export function IconSend(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+<path d="M20 12H4" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M13 19L20 12L13 5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+  );
+}
