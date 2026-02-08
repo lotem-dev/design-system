4744c49e1eeb23deee0ac0683e6edace6961c4b2
lotem-dev <lotem@jit.io>
2026-02-09 01:43:15 +0200

    Create IconWeb.tsx

---
+++ components/icons/resources_internal/IconWeb.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+export function IconWeb(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 28 28"
+      fill="none"
+      aria-hidden="true"
+      focusable="false"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M14 26.1333C20.7011 26.1333 26.1333 20.7011 26.1333 14C26.1333 7.29894 20.7011 1.86667 14 1.86667C7.29895 1.86667 1.86667 7.29894 1.86667 14C1.86667 20.7011 7.29895 26.1333 14 26.1333Z"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.86667 14H26.1333"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 1.86667C17.2241 5.38487 19.0681 9.97879 19.1667 14C19.0681 18.0212 17.2241 22.6151 14 26.1333C10.7759 22.6151 8.93194 18.0212 8.83334 14C8.93194 9.97879 10.7759 5.38487 14 1.86667Z"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
