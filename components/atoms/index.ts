b2cdf1ae82c455cdb20d8841e716c51a5caa796c
lotem-dev <lotem@jit.io>
2026-02-08 23:52:44 +0200

    Update index.ts

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -14,3 +14,6 @@ export type { IconSize } from "./IconWrapper";
 
 export { BrandLogoWrapper } from "./BrandLogoWrapper";
 export type { BrandLogoSize } from "./BrandLogoWrapper";
+
+export { Divider } from "./Divider";
+export type { DividerOrientation } from "./Divider";
