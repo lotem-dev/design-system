345c01658a9f03092f9a7f86ac1347cc1c234fdd
lotem-dev <lotem@jit.io>
2026-02-09 02:11:49 +0200

    Create index.ts

---
+++ components/icons/chevrons/index.ts
@@ -0,0 +1,4 @@
+export { IconChevronDown } from "./IconChevronDown";
+export { IconChevronUp } from "./IconChevronUp";
+export { IconChevronLeft } from "./IconChevronLeft";
+export { IconChevronRight } from "./IconChevronRight";
