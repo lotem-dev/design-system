d4b123a0701578ac5130c17178d8882a317f6590
lotem-dev <lotem@jit.io>
2026-02-09 02:19:09 +0200

    Create IconDropdownIndicator.tsx

---
+++ components/icons/dropdown/IconDropdownIndicator.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+export function IconDropdownIndicator(
+  props: React.SVGProps<SVGSVGElement>
+) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      aria-hidden="true"
+      focusable="false"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M6 9L12 15L18 9"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
