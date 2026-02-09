359371eb943eeacde99d16c61b700fd21f120f5d
lotem-dev <lotem@jit.io>
2026-02-09 13:55:49 +0200

    Update index.ts

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,12 +1,3 @@
-export { Text } from "./Text";
-export type {
-  TextVariant,
-  TextWeight,
-  TextDecoration,
-  TextTransform,
-  TextColor,
-} from "./Text";
-
 export { Link } from "./Link";
 
 export { IconWrapper } from "./IconWrapper";
