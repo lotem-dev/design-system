c186e7ae619ee9be25ad3adbee1fd5b0cea5535c
lotem-dev <lotem@jit.io>
2026-03-21 17:33:59 +0200

    fix: bake 12x19 inline styles into sort and dropdown icon components

--- components/icons/sorting/IconSortDefault.tsx
+++ components/icons/sorting/IconSortDefault.tsx
@@ -10,6 +10,7 @@ export function IconSortDefault(props: React.SVGProps<SVGSVGElement>) {
       focusable="false"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
+      style={{ width: 12, height: 19, flexShrink: 0, ...props.style }}
     >
       <path d="M2 8L6 5L10 8" stroke="var(--text-secondary)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
       <path d="M2 11L6 14L10 11" stroke="var(--text-secondary)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
